a988e943b2f0d726760e072364fcce099667dd6d
Naomi <naomi.paulmin@gmail.com>
2021-01-24 14:52:09 +0100

    feat: add localized name in returned categories

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -1,7 +1,12 @@
 import { Document } from 'mongoose'
 
+export interface ILabel {
+  locale: string
+  value: string
+}
+
 export interface ICategory extends Document {
   slug: string
   isActivated: boolean
-  labels: Array<Object>
+  labels: Array<ILabel>
 }
